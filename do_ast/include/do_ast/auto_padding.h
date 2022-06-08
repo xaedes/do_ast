aa918872b6e022f61733a25ae9d5fdb79700d301
xaedes <xaedes@gmail.com>
2022-06-08 18:38:19 +0200

    fix bug: when padding is not required - BUT then unnecessary padding is added
    this is because the padding array will have at least a size of 1 and std::enable_if cannot be used, as it requires an actual type.

--- do_ast/include/do_ast/auto_padding.h
+++ do_ast/include/do_ast/auto_padding.h
@@ -3,14 +3,18 @@
 #include <array>
 #include <cstdint>
 #include <type_traits>
+#include <string>
 
 namespace do_ast {
 
     template<uint32_t SizeOfPayload, uint32_t Alignment = 16>
-    using AutoPadding = std::enable_if_t<
-        ((SizeOfPayload % Alignment) != 0),
-        std::array<uint8_t, Alignment - (SizeOfPayload % Alignment)>
+    //using AutoPadding = std::enable_if_t<
+    //    ((SizeOfPayload % Alignment) != 0),
+    //    std::array<uint8_t, Alignment - (SizeOfPayload % Alignment)>
+    //>;
+    using AutoPadding = std::array<
+        uint8_t, 
+        ((SizeOfPayload % Alignment) == 0) ? Alignment : Alignment - (SizeOfPayload % Alignment)
     >;
-
 
 } // namespace do_ast
